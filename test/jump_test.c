858201998a28bc1cf6b397b7a970e3feef4691d7
Louis Taylor <louis@kragniz.eu>
2018-10-14 18:20:29 +0100

    Add extra jump tests

--- test/jump_test.c
+++ test/jump_test.c
@@ -8,11 +8,21 @@ int tests_run = 0;
 
 static char *test_jump()
 {
+	/* test values taken from lithammer/python-jump-consistent-hash */
 	int32_t h = jump_consistent_hash(1, 1);
 	mshrm_assert("error, jump_consistent_hash != 0", h == 0);
 
 	h = jump_consistent_hash(42, 57);
 	mshrm_assert("error, jump_consistent_hash != 43", h == 43);
+
+	h = jump_consistent_hash(0xDEAD10CC, 1);
+	mshrm_assert("error, jump_consistent_hash != 0", h == 0);
+
+        h = jump_consistent_hash(0xDEAD10CC, 666);
+	mshrm_assert("error, jump_consistent_hash != 361", h == 361);
+
+        h = jump_consistent_hash(256, 1024);
+	mshrm_assert("error, jump_consistent_hash != 520", h == 520);
 	return 0;
 }
 
