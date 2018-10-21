2f70a689629c401619694e9811cdea38feee5c61
Louis Taylor <louis@kragniz.eu>
2018-10-21 17:50:53 +0100

    Add basic udp server

--- mushroom.c
+++ mushroom.c
@@ -6,48 +6,55 @@
 
 #define PROJECT_NAME "mushroom"
 
-uv_loop_t *loop;
-uv_udp_t recv_socket;
+#define CHECK(r, msg)                                                          \
+	if (r < 0) {                                                           \
+		fprintf(stderr, "%s: %s\n", msg, uv_strerror(r));              \
+		exit(1);                                                       \
+	}
 
-void alloc_buffer(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf)
+static uv_loop_t *uv_loop;
+static uv_udp_t server;
+
+static void on_recv(uv_udp_t *handle, ssize_t nread, const uv_buf_t *rcvbuf,
+		    const struct sockaddr *addr, unsigned flags)
+{
+	if (nread > 0) {
+		printf("%lu\n", nread);
+		printf("%s", rcvbuf->base);
+	}
+	printf("free  :%lu %p\n", rcvbuf->len, (void *)rcvbuf->base);
+	free(rcvbuf->base);
+}
+
+static void on_alloc(uv_handle_t *client, size_t suggested_size, uv_buf_t *buf)
 {
 	buf->base = malloc(suggested_size);
 	buf->len = suggested_size;
-}
-
-void on_read(uv_udp_t *req, ssize_t nread, const uv_buf_t *buf,
-	     const struct sockaddr *addr, unsigned flags)
-{
-	if (nread < 0) {
-		fprintf(stderr, "Read error %s\n", uv_err_name(nread));
-		uv_close((uv_handle_t *)req, NULL);
-		free(buf->base);
-		return;
-	}
-
-	char sender[17] = { 0 };
-	uv_ip4_name((const struct sockaddr_in *)addr, sender, 16);
-	fprintf(stderr, "Recv from %s\n", sender);
-
-	free(buf->base);
-	uv_udp_recv_stop(req);
+	printf("malloc:%lu %p\n", buf->len, (void *)buf->base);
 }
 
 int main(int argc, char **argv)
 {
+	int status;
+	struct sockaddr_in addr;
+	uv_loop = uv_default_loop();
+
+	status = uv_udp_init(uv_loop, &server);
+	CHECK(status, "init");
+
+	uv_ip4_addr("0.0.0.0", 11000, &addr);
+
+	status = uv_udp_bind(&server, (const struct sockaddr *)&addr, 0);
+	CHECK(status, "bind");
+
+	status = uv_udp_recv_start(&server, on_alloc, on_recv);
+	CHECK(status, "recv");
+
 	if (argc != 1) {
 		printf("%s takes no arguments.\n", argv[0]);
 		return 1;
 	}
 	printf("This is project %s.\n", PROJECT_NAME);
-	loop = uv_default_loop();
 
-	uv_udp_init(loop, &recv_socket);
-	struct sockaddr_in recv_addr;
-	uv_ip4_addr("0.0.0.0", 6969, &recv_addr);
-	uv_udp_bind(&recv_socket, (const struct sockaddr *)&recv_addr,
-		    UV_UDP_REUSEADDR);
-	uv_udp_recv_start(&recv_socket, alloc_buffer, on_read);
-
-	return uv_run(loop, UV_RUN_DEFAULT);
+	return uv_run(uv_loop, UV_RUN_DEFAULT);
 }
