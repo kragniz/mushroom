d4d57c77c106a490b0a564131ae808dc49b0ac39
Louis Taylor <louis@kragniz.eu>
2018-10-23 22:33:49 +0100

    Init gossip server

--- mushroom.c
+++ mushroom.c
@@ -4,51 +4,15 @@
 
 #include <uv.h>
 
+#include "gossip_server.h"
+
 #define PROJECT_NAME "mushroom"
-
-#define CHECK(r, msg)                                                                              \
-	if (r < 0) {                                                                               \
-		fprintf(stderr, "%s: %s\n", msg, uv_strerror(r));                                  \
-		exit(1);                                                                           \
-	}
-
-static uv_loop_t *uv_loop;
-static uv_udp_t server;
-
-static void on_recv(uv_udp_t *handle, ssize_t nread, const uv_buf_t *rcvbuf,
-		    const struct sockaddr *addr, unsigned flags)
-{
-	if (nread > 0) {
-		printf("%lu\n", nread);
-		printf("%s", rcvbuf->base);
-	}
-	printf("free  :%lu %p\n", rcvbuf->len, (void *)rcvbuf->base);
-	free(rcvbuf->base);
-}
-
-static void on_alloc(uv_handle_t *client, size_t suggested_size, uv_buf_t *buf)
-{
-	buf->base = malloc(suggested_size);
-	buf->len = suggested_size;
-	printf("malloc:%lu %p\n", buf->len, (void *)buf->base);
-}
 
 int main(int argc, char **argv)
 {
-	int status;
-	struct sockaddr_in addr;
-	uv_loop = uv_default_loop();
+	uv_loop_t *uv_loop = uv_default_loop();
 
-	status = uv_udp_init(uv_loop, &server);
-	CHECK(status, "init");
-
-	uv_ip4_addr("0.0.0.0", 11000, &addr);
-
-	status = uv_udp_bind(&server, (const struct sockaddr *)&addr, 0);
-	CHECK(status, "bind");
-
-	status = uv_udp_recv_start(&server, on_alloc, on_recv);
-	CHECK(status, "recv");
+	mushroom_gossip_server_new(uv_loop, 6969);
 
 	if (argc != 1) {
 		printf("%s takes no arguments.\n", argv[0]);
