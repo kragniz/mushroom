d1eaf66b7ec008f40549fdba2220e50026816461
Louis Taylor <louis@kragniz.eu>
2018-10-25 00:11:07 +0100

    Add log levels

--- include/log.h
+++ include/log.h
@@ -2,6 +2,14 @@
 
 #include <stdarg.h>
 
-#define mushroom_log_debug(...) mushroom_log(__FILE__, __LINE__, __VA_ARGS__)
+enum mushroom_log_level {
+	MUSHROOM_LOG_FATAL,
+	MUSHROOM_LOG_ERROR,
+	MUSHROOM_LOG_WARNING,
+	MUSHROOM_LOG_INFO,
+	MUSHROOM_LOG_DEBUG
+};
 
-void mushroom_log(const char *file, int line, const char *format, ...);
+#define mushroom_log_debug(...) mushroom_log(MUSHROOM_LOG_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
+
+void mushroom_log(int level, const char *file, int line, const char *format, ...);
--- log.c
+++ log.c
@@ -4,15 +4,15 @@
 
 #include "log.h"
 
-#define timestamp_buffer_size 32
+static const char *level_names[] = { "fatal", "error", "warning", "info", "debug" };
 
-void mushroom_log(const char *file, int line, const char *format, ...)
+void mushroom_log(int level, const char *file, int line, const char *format, ...)
 {
 	struct timespec tms;
 	timespec_get(&tms, TIME_UTC);
 
 	printf("[%li.%li] %s%-5s\x1b[0m \x1b[90m%s:%d:\x1b[0m ", tms.tv_sec, tms.tv_nsec / 10000000,
-	       "\x1b[94m", "debug", file, line);
+	       "\x1b[94m", level_names[level], file, line);
 
 	va_list arglist;
 	va_start(arglist, format);
