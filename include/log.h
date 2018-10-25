d6fcddeb4128bbef6e073b4fa69fedb4eaaf2f15
Louis Taylor <louis@kragniz.eu>
2018-10-26 00:03:55 +0100

    Add log levels

--- include/log.h
+++ include/log.h
@@ -5,11 +5,15 @@
 enum mushroom_log_level {
 	MUSHROOM_LOG_FATAL,
 	MUSHROOM_LOG_ERROR,
-	MUSHROOM_LOG_WARNING,
+	MUSHROOM_LOG_WARN,
 	MUSHROOM_LOG_INFO,
 	MUSHROOM_LOG_DEBUG
 };
 
 #define mushroom_log_debug(...) mushroom_log(MUSHROOM_LOG_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
+#define mushroom_log_info(...) mushroom_log(MUSHROOM_LOG_INFO, __FILE__, __LINE__, __VA_ARGS__)
+#define mushroom_log_warn(...) mushroom_log(MUSHROOM_LOG_WARN, __FILE__, __LINE__, __VA_ARGS__)
+#define mushroom_log_error(...) mushroom_log(MUSHROOM_LOG_ERROR, __FILE__, __LINE__, __VA_ARGS__)
+#define mushroom_log_fatal(...) mushroom_log(MUSHROOM_LOG_FATAL, __FILE__, __LINE__, __VA_ARGS__)
 
 void mushroom_log(int level, const char *file, int line, const char *format, ...);
--- log.c
+++ log.c
@@ -4,15 +4,21 @@
 
 #include "log.h"
 
-static const char *level_names[] = { "fatal", "error", "warning", "info", "debug" };
+static const char *level_names[] = {"fatal", "error", "warning", "info", "debug"};
+
+static int log_level = MUSHROOM_LOG_DEBUG;
 
 void mushroom_log(int level, const char *file, int line, const char *format, ...)
 {
+	if (level > log_level) {
+		return;
+	}
+
 	struct timespec tms;
 	timespec_get(&tms, TIME_UTC);
 
-	printf("[%li.%li] %s%-5s\x1b[0m \x1b[90m%s:%d:\x1b[0m ", tms.tv_sec, tms.tv_nsec / 10000000,
-	       "\x1b[94m", level_names[level], file, line);
+	printf("[%li.%li] %s%-5s\x1b[0m\t\x1b[90m%s:%d:\x1b[0m ", tms.tv_sec,
+	       tms.tv_nsec / 10000000, "\x1b[94m", level_names[level], file, line);
 
 	va_list arglist;
 	va_start(arglist, format);
