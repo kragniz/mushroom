a85ab582b8eff148a565c77c34a0c7b5ec8b1c78
Louis Taylor <louis@kragniz.eu>
2018-10-28 19:38:18 +0000

    Prettier logs

--- include/log.h
+++ include/log.h
@@ -10,11 +10,17 @@ enum mushroom_log_level {
 	MUSHROOM_LOG_DEBUG
 };
 
-#define mushroom_log_debug(...) mushroom_log(MUSHROOM_LOG_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
-#define mushroom_log_info(...) mushroom_log(MUSHROOM_LOG_INFO, __FILE__, __LINE__, __VA_ARGS__)
-#define mushroom_log_warn(...) mushroom_log(MUSHROOM_LOG_WARN, __FILE__, __LINE__, __VA_ARGS__)
-#define mushroom_log_error(...) mushroom_log(MUSHROOM_LOG_ERROR, __FILE__, __LINE__, __VA_ARGS__)
-#define mushroom_log_fatal(...) _mushroom_log_fatal(__FILE__, __LINE__, __VA_ARGS__)
+#define _prefix 3
+
+#define mushroom_log_debug(...)                                                                    \
+	mushroom_log(MUSHROOM_LOG_DEBUG, __FILE__ + _prefix, __LINE__, __VA_ARGS__)
+#define mushroom_log_info(...)                                                                     \
+	mushroom_log(MUSHROOM_LOG_INFO, __FILE__ + _prefix, __LINE__, __VA_ARGS__)
+#define mushroom_log_warn(...)                                                                     \
+	mushroom_log(MUSHROOM_LOG_WARN, __FILE__ + _prefix, __LINE__, __VA_ARGS__)
+#define mushroom_log_error(...)                                                                    \
+	mushroom_log(MUSHROOM_LOG_ERROR, __FILE__ + _prefix, __LINE__, __VA_ARGS__)
+#define mushroom_log_fatal(...) _mushroom_log_fatal(__FILE__, __LINE__ + _prefix, __VA_ARGS__)
 
 void mushroom_log(int level, const char *file, int line, const char *format, ...);
 void _mushroom_log_fatal(const char *file, int line, const char *format, ...);
