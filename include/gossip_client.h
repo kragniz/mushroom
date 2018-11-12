d96ea0411c6148c4de8f3efe60bc597e46cffa3b
Louis Taylor <louis@kragniz.eu>
2018-11-12 20:54:00 +0000

    Correctly include and link flatc generated files

--- include/gossip_client.h
+++ include/gossip_client.h
@@ -2,12 +2,15 @@
 
 #include <uv.h>
 
+#include "gossip_builder.h"
 #include "ring.h"
 
 struct mushroom_gossip_client {
 	uv_loop_t *loop;
 	uv_timer_t *timer;
 	struct mushroom_ring *ring;
+
+	flatcc_builder_t *builder;
 };
 
 struct mushroom_gossip_client *mushroom_gossip_client_new(uv_loop_t *loop,
