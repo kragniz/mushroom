abccce40a87ccc6af67af077757e7921c899db07
Louis Taylor <louis@kragniz.eu>
2018-11-12 23:16:37 +0000

    Pass client to timer callback

--- src/gossip_client.c
+++ src/gossip_client.c
@@ -2,6 +2,7 @@
 
 #include <uv.h>
 
+#include "gossip_builder.h"
 #include "gossip_client.h"
 #include "log.h"
 #include "ring.h"
@@ -9,17 +10,26 @@
 static void gossip_event_callback(uv_timer_t *handle)
 {
 	mushroom_log_debug("gossiping");
+	struct mushroom_gossip_client *client = (struct mushroom_gossip_client *)handle->data;
+
+	if (client->ring->node_count <= 0) {
+		mushroom_log_debug("no nodes to gossip with");
+	}
 }
 
 struct mushroom_gossip_client *mushroom_gossip_client_new(uv_loop_t *loop,
 							  struct mushroom_ring *ring)
 {
 	struct mushroom_gossip_client *client = calloc(1, sizeof(*client));
-	client->timer = calloc(1, sizeof(*client->timer));
 	client->loop = loop;
 	client->ring = ring;
 
+	client->builder = malloc(sizeof(*client->builder));
+	flatcc_builder_init(client->builder);
+
+	client->timer = malloc(sizeof(*client->timer));
 	uv_timer_init(loop, client->timer);
+	client->timer->data = (void *)client;
 
 	mushroom_log_debug("created mushroom_gossip_client: %p", client);
 
