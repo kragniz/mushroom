a485b7791e2a26850fc033b2f760fdc5d41fb37c
Louis Taylor <louis@kragniz.eu>
2018-11-12 22:28:20 +0000

    Fix clang-check

--- hack/clang-check.sh
+++ hack/clang-check.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env sh
 
 cd "${MESON_SOURCE_ROOT}"
-clang-check -fixit -analyze src/*.c include/*.h test/*.c -- -Iinclude -Ibuild -Itest/deps/greatest
+clang-check -fixit -analyze src/*.c include/*.h test/*.c -- \
+    -Iinclude \
+    -Ibuild \
+    -Itest/deps/greatest \
+    -Ibuild/mushroom@exe/ \
+    -Isubprojects/flatcc/rules \
+    -Isubprojects/flatcc/include/ \
+    -Isubprojects/flatcc/include/flatcc/reflection/
