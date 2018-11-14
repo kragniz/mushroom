4d6951b2cc2e194f58ab8b000b5014b1c7313adc
Louis Taylor <louis@kragniz.eu>
2018-11-14 01:06:44 +0000

    Add leading zeros to timestamp

--- src/log.c
+++ src/log.c
@@ -15,7 +15,7 @@ mushroom_log_msg(int level, const char *file, int line, const char *format, va_l
 	struct timespec tms;
 	timespec_get(&tms, TIME_UTC);
 
-	printf("[%li.%li] %s%-5s\x1b[0m\t\x1b[90m%s:%d:\x1b[0m\t", tms.tv_sec,
+	printf("[%li.%02li] %s%-5s\x1b[0m\t\x1b[90m%s:%d:\x1b[0m\t", tms.tv_sec,
 	       tms.tv_nsec / 10000000, "\x1b[94m", level_names[level], file, line);
 
 	vprintf(format, argp);
