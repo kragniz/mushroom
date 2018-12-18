cffc167f6d9878600f98ce9e6f192a5c8d5e1879
Louis Taylor <louis@kragniz.eu>
2018-12-18 18:50:10 +0530

    Add mushroom_map_item

--- include/map.h
+++ include/map.h
@@ -1,8 +1,14 @@
 #pragma once
 
+struct mushroom_map_item {
+	char *key;
+	char *value;
+};
+
 struct mushroom_map {
 	int size;
 	int count;
+	struct mushroom_map_item **items;
 };
 
 struct mushroom_map *mushroom_map_new();
