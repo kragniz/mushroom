58f240bd1390faf13acf817b9dab7865d97f75f7
Louis Taylor <louis@kragniz.eu>
2019-01-12 00:16:05 +0000

    Start sending gossip responses

--- src/gossip_server.c
+++ src/gossip_server.c
@@ -13,6 +13,7 @@
 struct client {
 	uv_tcp_t handle;
 	uv_buf_t buf;
+	flatcc_builder_t *builder;
 };
 
 static void handle_gossip_message(mushroom_gossip_message_table_t *msg)
@@ -36,10 +37,52 @@ static void on_close(uv_handle_t *handle)
 	free(client);
 }
 
+static void after_write(uv_write_t *req, int status)
+{
+	assert(status == 0);
+	uv_close((uv_handle_t *)req->handle, on_close);
+	free(req);
+}
+
+static void write_gossip_message(struct client *client, uv_stream_t *stream)
+{
+	uv_buf_t gossip_msg;
+	on_alloc(NULL, 256, &gossip_msg);
+	memset(gossip_msg.base, 0, gossip_msg.len);
+
+	mushroom_join_response_ref_t join_response = mushroom_join_response_create(client->builder);
+	mushroom_message_contents_union_ref_t contents =
+		mushroom_message_contents_as_join_response(join_response);
+	mushroom_gossip_message_create_as_root(client->builder, 7070, contents);
+
+	size_t size = flatcc_builder_get_buffer_size(client->builder);
+	if (size >= 256)
+		mushroom_log_error("flatcc buffer too big: %d", size);
+	flatcc_builder_copy_buffer(client->builder, gossip_msg.base, size);
+
+	char buf[1024];
+	flatcc_json_printer_t *ctx = malloc(sizeof(*ctx));
+	flatcc_json_printer_init_buffer(ctx, buf, 1024);
+	gossip_print_json(ctx, gossip_msg.base, size);
+	flatcc_json_printer_flush(ctx);
+	if (flatcc_json_printer_get_error(ctx)) {
+		mushroom_log_error("could not print json");
+	}
+	free(ctx);
+	mushroom_log_debug("sending gossip response: %s", buf);
+
+	uv_write_t *req = malloc(sizeof(*req));
+	if (uv_write(req, stream, &gossip_msg, 1, after_write)) {
+		mushroom_log_error("uv_write failed");
+	}
+
+	flatcc_builder_reset(client->builder);
+}
+
 static void on_write(uv_write_t *write_req, int status)
 {
-	uv_close((uv_handle_t *)write_req->handle, on_close);
-	free(write_req);
+	struct client *client = (struct client *)write_req->data;
+	write_gossip_message(client, write_req->handle);
 }
 
 static void on_shutdown(uv_shutdown_t *shutdown_req, int status)
@@ -62,6 +105,7 @@ static void on_read(uv_stream_t *handle, ssize_t nread, const uv_buf_t *buf)
 
 		uv_write_t *write_req = malloc(sizeof(uv_write_t));
 		client->buf = uv_buf_init(buf->base, nread);
+		write_req->data = client;
 		int err = uv_write(write_req, (uv_stream_t *)&client->handle, &client->buf, 1,
 				   on_write);
 		if (err < 0) {
@@ -79,6 +123,8 @@ static void on_read(uv_stream_t *handle, ssize_t nread, const uv_buf_t *buf)
 
 void on_connection(uv_stream_t *server, int status)
 {
+	flatcc_builder_t *builder = (flatcc_builder_t *)server->data;
+
 	mushroom_log_debug("on_connection: client connected");
 	struct client *client = calloc(1, sizeof(*client));
 	int err = uv_tcp_init(server->loop, &client->handle);
@@ -87,6 +133,7 @@ void on_connection(uv_stream_t *server, int status)
 	}
 
 	client->handle.data = client;
+	client->builder = builder;
 
 	err = uv_accept(server, (uv_stream_t *)&client->handle);
 	if (err) {
@@ -112,6 +159,7 @@ mushroom_gossip_server_new(uv_loop_t *loop, const char *addr, int port)
 
 	server->builder = malloc(sizeof(*server->builder));
 	flatcc_builder_init(server->builder);
+	server->server->data = server->builder;
 
 	server->loop = loop;
 
